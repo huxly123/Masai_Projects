a75e5501c078cf331db04871d9b5afbb3976e723
huxly123 <87421772+huxly123@users.noreply.github.com>
2021-08-13 20:21:44 +0530

    Update copy.js

--- unit-2/sprint-2/day-3/assignments/copy.js
+++ unit-2/sprint-2/day-3/assignments/copy.js
@@ -1,6 +1,11 @@
 var arrayy=[0,1,2,3];
+var cpy=[];
+for(var i=0;i<arrayy.length;i++){
+  cpy.push(arrayy[i]);
+}
+
 function find(arr){
 arr[0]="masai";    //I am changing the first element of array inside function . 
 }
-console.log(arrayy);   //it returns the global value,so it doesn't affected by the manipulation 
+console.log(cpy);   //it returns the global value,so it doesn't affected by the manipulation 
                     //inside the function
