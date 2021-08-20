f8b3f4cb31ae6a8e748f29971aac12838dc8edd2
huxly123 <huxlygerard5@gmail.com>
2021-08-20 23:09:27 +0530

    fff

---
+++ unit-2/sprint-3/day-2/you/data.js
@@ -0,0 +1,12 @@
+/*primitive
+sting number null boolean undefined
+var  a
+non primitive 
+array object and function*/
+
+var a="sting"
+ a[0]="sss";
+ console.log(a);
+ var b=[1,2,3]
+ b[0]="strring";
+ console.log(b);
