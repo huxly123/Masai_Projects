805938e53a945058eef06132f7acc0e8e4a28aa4
huxly123 <huxlygerard5@gmail.com>
2021-12-03 16:05:31 +0530

    fdfsv

---
+++ unit-5/sprint-3/day-4/we/Typescript/index.js
@@ -0,0 +1,6 @@
+var a = 3;
+var b = 4;
+(function (a, b) {
+    return a + b;
+});
+console.log(gett(a, b));
---
+++ unit-5/sprint-3/day-4/we/Typescript/index.ts
@@ -0,0 +1,8 @@
+let a: number = 3;
+let b:number=4
+function gett : string(a,b) {
+    return a+b
+}
+
+console.log(gett(a,b));
+
