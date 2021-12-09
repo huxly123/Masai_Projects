246464e0a28df9e48102e44236973a3fd2f6f026
huxly123 <huxlygerard5@gmail.com>
2021-12-09 22:36:48 +0530

    fds

--- unit-5/sprint-4/day-3 testing3/we/test/cypress/integration/todo.test.js
+++ unit-5/sprint-4/day-3 testing3/we/test/cypress/integration/todo.test.js
@@ -1,10 +1,29 @@
 ///<reference types="cypress"/>
 
 describe("todo test", function () {
+
+    beforeEach(function () {
+    cy.visit("http://localhost:3000/")
+})
+
   it("goto todos app", function () {
       cy.visit("http://localhost:3000/");
       
       cy.get("input").should("exist")
-      cy.get("button")
+      cy.get("button").should("contain","")
   })
+    it("should type in input box", function () {
+        cy.get("input").type("Learn React").should("have.value","Learn React")
+    })
+    
+
+   it("todo should get added", function () {
+     cy.get("input").type("Learn React").should("have.value", "Learn React");
+   
+       cy.get("button").click()
+       
+    //    cy.visit("/users")
+   
+   });
+
 })
