389c47d2af20724b4aed760af45ff625ea0c6c9f
huxly123 <huxlygerard5@gmail.com>
2021-12-09 22:26:34 +0530

    fds

--- unit-5/sprint-4/day-3 testing3/we/test/cypress/integration/todo.test.js
+++ unit-5/sprint-4/day-3 testing3/we/test/cypress/integration/todo.test.js
@@ -2,6 +2,9 @@
 
 describe("todo test", function () {
   it("goto todos app", function () {
-    cy.visit("http://localhost:3000/");
-  });
-});
+      cy.visit("http://localhost:3000/");
+      
+      cy.get("input").should("exist")
+      cy.get("button")
+  })
+})
