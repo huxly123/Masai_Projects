57f9e5f30baf588f1f5e567a6cfe56492f49dbd0
huxly123 <huxlygerard5@gmail.com>
2021-12-10 02:15:52 +0530

    fds

--- unit-5/sprint-4/day-3 cypress/you/cyper/cypress/integration/input.test.js
+++ unit-5/sprint-4/day-3 cypress/you/cyper/cypress/integration/input.test.js
@@ -1,12 +1,25 @@
 
 describe("test", function () {
-  beforeEach(function () {
-    cy.visit("http://localhost:3000/");
-  });
-  xit("auto focus", function () {
-    cy.get("input").should("exist")
-  });
-xit("type", function () {
-  cy.get(".inp").type("Learn").should("have.value", "Learn");
-});
-});
+    beforeEach(function () {
+        cy.visit("http://localhost:3000/");
+    });
+    xit("auto focus", function () {
+        cy.get("input").should("exist")
+    });
+    xit("type", function () {
+        cy.get(".inp").type("Learn").should("have.value", "Learn");
+    });
+    
+    it("button", function () {
+        cy.get("button").first().should("exist")
+    })
+    
+    it("del", function () {
+        cy.request("DELETE", "https://reqres.in/api/users/2").then((data) => {
+            cy.log(data);
+
+            expect(data.status).to.eq(204);
+            
+        })
+    })
+})
--- unit-5/sprint-4/day-3 cypress/you/cyper/src/components/Input.jsx
+++ unit-5/sprint-4/day-3 cypress/you/cyper/src/components/Input.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function Input() {
+
+  
+
     return (
         <div>
             <input autoFocus className='inp' type="text" placeholder='Enter input' />
+            <button className='="btn' >Delete</button>
         </div>
     )
 }
