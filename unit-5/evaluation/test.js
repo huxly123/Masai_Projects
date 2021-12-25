9f24aa2f3ca56bcae5e365d3437a3bd594f7a0a8
huxly123 <huxlygerard5@gmail.com>
2021-12-26 02:22:02 +0530

    profile

--- unit-5/evaluation/test.js
+++ unit-5/evaluation/test.js
@@ -1,5 +1,26 @@
-const randomNumbers = [4, 11, 42, 14, 39];
-const filteredArray = randomNumbers.filter((n) => {
-  return n > 5;
-});
-console.log(filteredArray);
+//  var x = 100;
+// function test() {
+//     console.log(x);
+//     let  x=i=10
+// }
+// test()
+// // console.log(x, i);
+
+// dett()
+// function dett() {
+//     isv = "gef"
+//     var isv
+//     console.log(isv);
+// }
+
+// var a = "adssa"
+
+// function dd() {
+//     var a;
+//     console.log(a);
+// }
+// dd()
+
+a = "sfs"
+var a;
+console.log(a);
