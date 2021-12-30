4bf168d1d9b29536bae09498163accfe4dd666a3
huxly123 <huxlygerard5@gmail.com>
2021-12-30 10:07:36 +0530

    json

--- unit-5/evaluation/sidelane.js
+++ unit-5/evaluation/sidelane.js
@@ -1,56 +1,35 @@
 function runProgram(input) {
-    input = input.trim().split("\n");
-    let line = 0
-    while (true) {
-        let n = +(input[line++]);
-       
-        if (n == 0) {
-            break;
-        }
-        let arr = input[line++].trim().split(" ").map(Number);
-        let temp = [];
-        let ans = []
-        while (arr.length > 0) {
-            if (arr[0] > arr[1]) {
-                temp.push(arr.shift())
+  input = input.trim().split("\n");
+  let line = 0;
+  while (true) {
+    let n = +input[line++];
 
-            } else {
-                ans.push(arr.shift())
-            }
-        }
-        ans = ans.concat(temp)
-        let occ = true;
-        console.log(ans);
-        for (let i = 0; i < ans.length - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                occ = false;
-                break
-            }
-        }
-        // if (occ == true) {
-        //     console.log("yes");
-        // } else {
-        //     console.log("no");
-        // }
+    if (n == 0) {
+      break;
     }
+    let arr = input[line++].trim().split(" ").map(Number);
+    let temp = [];
+    let ans = [];
+    while (arr.length !== 0) {
+                                                                //4 1 5 3 2
+      // console.log(arr,temp);
+
+      if (arr[arr.length - 1] > arr[arr.length - 2]) {
+        temp.push(arr.pop());
+      } else if (arr[arr.length - 1] < arr[arr.length - 2]) {
+        ans.push(arr.pop());
+      } else if (temp[temp.length - 1] < arr[arr.length - 1]) {
+        ans.push(temp.pop());
+      } else if (arr.length == 1 && temp.length == 0) {
+        ans.push(arr.pop());
+      }
+    }
+    console.log(ans);
+  }
 }
 if (process.env.USERNAME === "huxly") {
   runProgram(`5 
 4 1 5 3 2 
-5 
-3 1 2 5 4 
-5 
-5 3 2 1 4 
-10 
-1 2 10 5 4 3 7 6 8 9 
-10 
-1 2 10 5 4 3 9 8 7 6 
-5 
-3 5 2 4 1 
-5 
-1 2 4 3 5 
-4 
-4 2 3 1 
 0`);
 } else {
   process.stdin.resume();
@@ -70,3 +49,17 @@ if (process.env.USERNAME === "huxly") {
     process.exit(0);
   });
 }
+// 5 
+// 3 1 2 5 4 
+// 5 
+// 5 3 2 1 4 
+// 10 
+// 1 2 10 5 4 3 7 6 8 9 
+// 10 
+// 1 2 10 5 4 3 9 8 7 6 
+// 5 
+// 3 5 2 4 1 
+// 5 
+// 1 2 4 3 5 
+// 4 
+// 4 2 3 1 
--- unit-5/evaluation/test.js
+++ unit-5/evaluation/test.js
@@ -53,10 +53,89 @@
 // }
 // nam.dd = 44
 // console.log(nam);
-let arr = [1, 34, 3, 21];
+// let arr = [1, 34, 3, 21];
 
-for (var i = 0; i < arr.length; i++){
-    setTimeout(() => {
-        console.log(arr[i],i);
-    },i*1000)
-}
+// for (var i = 0; i < arr.length; i++){
+//     setTimeout(() => {
+//         console.log(arr[i],i);
+//     },i*1000)
+// }
+
+// function init() {
+//   const name = "Mozilla"; // name is a local variable created by init
+//   function displayName() {
+//     // displayName() is the inner function, a closure
+//    console.log(name); // use variable declared in the parent function
+//   }
+//   displayName();
+// }
+// init();
+                               //clos doubt
+// function makeFunc() {
+//   const name = "Mozilla";
+//   function displayName() {
+//     console.log(name);
+//   }
+//   return displayName;
+// }
+
+// // const myFunc = makeFunc();
+// makeFunc();
+
+// let makeFunc=()=> {
+//   const name = "Mozilla";
+//   function displayName() {
+//     console.log(name);
+//   }
+//   return displayName;
+// }
+
+//  const myFunc = makeFunc();
+// myFunc();
+
+// function makeAdder(x) {
+//     console.log("x",x);
+//     return function (y) {
+//       console.log("y", y);
+//     return x + y;
+//   };
+// }
+
+// var add5 = makeAdder(5);
+// var add10 = makeAdder(10);
+
+// console.log(add5(2));  // 7
+// console.log(add10(2)); // 12
+
+// function x() {
+//     for (var i = 1; i <= 5; i++){
+//         setTimeout(() => {
+//             console.log(i);
+//         },i*1000)
+//     }
+// }
+// x()
+// console.log("ff");
+// "use strict"
+// x = 5
+// // console.log(x);
+//     let a = 1;
+//     function outer() {
+    
+//         function inner(b) {
+//             console.log(a+b);
+//         }
+//         return inner
+//     }
+    
+// outer()(3)
+// function greeting(name) {
+//  console.log(name+1);
+// }
+
+// function processUserInput(callback) {
+//   var name = "Please enter your name."
+//   callback(name);
+// }
+
+// processUserInput(greeting);
